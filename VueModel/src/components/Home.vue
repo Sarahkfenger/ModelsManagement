3e7d0fb256d566bcd2a0651569689fec287b7fe7
Sarah Krohn Fenger <au577425@uni.au.dk>
2020-05-11 14:09:11 +0200

    Tilføjet loginSide

--- VueModel/src/components/Home.vue
+++ VueModel/src/components/Home.vue
@@ -1,7 +1,14 @@
 <template>
-    <div class="home">
-        <h1>{{ msg }}</h1>
-        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a>.</p>
+    <div class="home" id="login">
+        <h1>Welcome to Model Management</h1>
+        <p>First you need to login</p>
+        <input type="text" name="username" v-model="input.username" placeholder="Username"/>
+        <br/>
+        <br/>
+        <input type="password" name="password" v-model="input.password" placeholder="Password"/>
+        <br/>
+        <br/>
+        <button type="button" v-on:click="login()">Login</button>
     </div>
 </template>
 
@@ -9,12 +16,46 @@
     export default {
         name: 'Home',
         props: {
-            msg: String
+        },
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: "",
+                }
+            }
+        }, 
+        methods: {
+            login() {
+                if (this.input.username != "" && this.input.password != "") {
+                    fetch('https://localhost:44368/api/Account/login', {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            email: this.input.username,
+                            password: this.input.password
+                        }),
+                        headers: new Headers({
+                            'Content-Type': 'application/json'
+                        })
+                    }).then(res => res.json().then(token => {
+                        localStorage.setItem("token", token.jwt);
+
+                    }))
+                }
+            }
         }
-    };
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
 </style>
 
