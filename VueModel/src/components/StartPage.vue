5df05c4faee9d842ddab477b4a174cbb059962a7
Sarah Krohn Fenger <au577425@uni.au.dk>
2020-05-13 14:23:13 +0200

    Tilføjet tjek om man er model eller manager

--- VueModel/src/components/StartPage.vue
+++ VueModel/src/components/StartPage.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
-        <h1>Hello, world!</h1>
+        <span v-if="isManager">Now you see me</span>
     </div>
 </template>
 
 <script>
     export default {
         name: "StartPage",
-        data() {
-            return {}
+
+        created() {
+            let jwt = localStorage.getItem("token");
+            let jwtData = jwt.split('.')[1]
+            let decodedJwtJsonData = window.atob(jwtData)
+            let decodedJwtData = JSON.parse(decodedJwtJsonData)
+
+            let role = decodedJwtData["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+
+            if (role == "Manager") {
+                this.isManager = true
+            }
+            else if (role == "Model") {
+                this.isModel = true
+            }
+        },
+
+        data: function () {
+            return {
+                isManager: false,
+                isModel: false,
+            }
         }
+    
     }
 </script>
 
